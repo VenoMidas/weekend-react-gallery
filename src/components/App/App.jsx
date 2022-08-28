fa25c076109f07396f133f9a799fb19ea8a78146
Mark Schumacher <schuma1022@gmail.com>
2022-08-28 10:55:17 -0500

    Add Axios GET

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import GalleryList from '../GalleryList/GalleryList.jsx';
 import GalleryItem from '../GalleryItem/GalleryItem.jsx';
+import Axios from 'axios';
 
 function App() {
+  // getters and setters
+  let [galleryArray, setGalleryArray] = useState([]);
+
+  // useEffect runs on page load
+  useEffect(() => {
+    // console.log('in useEffect');
+    getGallery();
+  });
+
+  const getGallery = () => {
+    Axios({
+      method: 'GET',
+      url: '/gallery',
+    }).then(response => {
+      setGalleryArray(response.data);
+      // console.log(galleryArray);
+    }).catch(error => {
+      console.log(error);
+      alert('Something went wrong!');
+    });
+  };
+
     return (
       <div className="App">
         <header className="App-header">
