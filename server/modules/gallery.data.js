29577ab2d346d664691b7dcc9a1182b1d979f6f5
Mark Schumacher <schuma1022@gmail.com>
2022-08-28 08:59:25 -0500

    Add obejects to galary data
    Need to verify path

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,9 +1,59 @@
 const galleryItems = [
-    { 
-        id: 1, 
-        path: 'images/goat_small.jpg', 
-        description: 'Photo of a goat taken at Glacier National Park.', 
-        likes: 0 }
+    {
+        id: 1,
+        title: 'universe',
+        description: 'Our whole universe was in a hot, dense state',
+        pathFor: '../../public/images/universe.png',
+    },
+    {
+        id: 2,
+        title: 'earth',
+        description: 'The earth began to cool',
+        pathFor: '../../public/images/earth.png',
+    },
+    {
+        id: 3,
+        title: 'autotroph',
+        description: 'the autotrophs began to drool',
+        pathFor: '../../public/images/autotroph.png',
+    },
+    {
+        id: 4,
+        title: 'neanderthals tools',
+        description: 'Neanderthals developed tools',
+        pathFor: '../../public/images/neanderthals_tools.png',
+    },
+    {
+        id: 5,
+        title: 'great wall',
+        description: 'We built a wall',
+        pathFor: '../../public/images/great_wall.png',
+    },
+    {
+        id: 6,
+        title: 'pyramids',
+        description: 'We built the pyramids',
+        pathFor: '../../public/images/pyramids.png',
+    },
+    {
+        id: 7,
+        title: 'mystery',
+        description: 'unraveling the mysteries',
+        pathFor: '../../public/images/mystery.png',
+    },
+    {
+        id: 8,
+        title: 'big bang',
+        description: 'That all started with the big bang',
+        pathFor: '../../public/images/big_bang.png',
+    },
 ];
 
 module.exports = galleryItems;
+
+// { 
+//     id: 1, 
+//     path: 'images/goat_small.jpg', 
+//     description: 'Photo of a goat taken at Glacier National Park.', 
+//     likes: 0 
+// }
