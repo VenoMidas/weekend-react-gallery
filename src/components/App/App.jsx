e96a67118cab6d744547131d9c19b3d394683876
Mark Schumacher <schuma1022@gmail.com>
2022-08-28 09:08:46 -0500

    Create GalleryList component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import GalleryList from '../GalleryList/GalleryList.jsx';
 
 function App() {
     return (
@@ -7,7 +8,7 @@ function App() {
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
+        <GalleryList />
         <img src="images/goat_small.jpg"/>
       </div>
     );
---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './GalleryList.css';
+
+function GalleryList() {
+    return (
+        <div>
+            <h2 className="galleryListTest">Gallery List</h2>
+        </div>
+    );
+};
+
+export default GalleryList;
